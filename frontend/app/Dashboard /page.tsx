a3ce5c88b6fbb76e7333f5cecd2714b83c33fa28
Brian chelule <116880584+Brian-17@users.noreply.github.com>
2026-08-02 06:09:19 +0300

    Add BullseyeSetupBuilder to DashboardPage

--- frontend/app/Dashboard /page.tsx
+++ frontend/app/Dashboard /page.tsx
@@ -1,4 +1,4 @@
-import EconomicCalendar from "../../components/dashboard/EconomicCalendar";import AISignalPanel from "../../components/dashboard/AISignalPanel";import MarketOverview from "../../components/dashboard/MarketOverview";import StatsCard from "../../components/dashboard/StatsCard";
+import BullseyeSetupBuilder from "../../components/dashboard/BullseyeSetupBuilder";import EconomicCalendar from "../../components/dashboard/EconomicCalendar";import AISignalPanel from "../../components/dashboard/AISignalPanel";import MarketOverview from "../../components/dashboard/MarketOverview";import StatsCard from "../../components/dashboard/StatsCard";
 import PerformanceChart from "../../components/dashboard/PerformanceChart";
 export default function DashboardPage() {
  import RecentTrades from "../../components/dashboard/RecentTrades";
@@ -31,6 +31,7 @@ export default function DashboardPage() {
 <RecentTrades />
 <AISignalPanel />
   <EconomicCalendar />
+   <BullseyeSetupBuilder />
     </main>
   );
 }
