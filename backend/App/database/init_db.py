96d4e02fa6b0c20c289666623edd6f782500fed2
Brian chelule <116880584+Brian-17@users.noreply.github.com>
2026-08-02 19:22:49 +0300

    Fix import paths in init_db.py

--- backend/App/database/init_db.py
+++ backend/App/database/init_db.py
@@ -1,10 +1,12 @@
-from app.database.database import engine
-from app.database.base import Base
+from App.database.database import engine
+from App.database.base import Base
 
-from app.models.trade import Trade
+from App.models.trade import Trade
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
 
 if __name__ == "__main__":
     init_db()
