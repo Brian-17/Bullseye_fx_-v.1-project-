a4bccff53a97849872bf074dd2df2d328de330ef
Brian chelule <116880584+Brian-17@users.noreply.github.com>
2026-08-02 13:27:13 +0300

    Import RiskRewardCalculator in Dashboard page
    Added RiskRewardCalculator import to Dashboard page.

--- frontend/app/Dashboard /page.tsx
+++ frontend/app/Dashboard /page.tsx
@@ -1,4 +1,9 @@
-import BullseyeSetupBuilder from "../../components/dashboard/BullseyeSetupBuilder";import EconomicCalendar from "../../components/dashboard/EconomicCalendar";import AISignalPanel from "../../components/dashboard/AISignalPanel";import MarketOverview from "../../components/dashboard/MarketOverview";import StatsCard from "../../components/dashboard/StatsCard";
+import RiskRewardCalculator from "../../components/dashboard/RiskRewardCalculator";
+import BullseyeSetupBuilder from "../../components/dashboard/BullseyeSetupBuilder";
+import EconomicCalendar from "../../components/dashboard/EconomicCalendar";
+import AISignalPanel from "../../components/dashboard/AISignalPanel";
+import MarketOverview from "../../components/dashboard/MarketOverview";
+import StatsCard from "../../components/dashboard/StatsCard";
 import PerformanceChart from "../../components/dashboard/PerformanceChart";
 export default function DashboardPage() {
  import RecentTrades from "../../components/dashboard/RecentTrades";
