dc03ce7b2bfb6e9b2b42e81541c5d3e201cd8657
Brian chelule <116880584+Brian-17@users.noreply.github.com>
2026-08-03 23:24:37 +0300

    Add user relationship to Trade model

--- backend/app/models/trade.py
+++ backend/app/models/trade.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 from app.database.database import Base
 
@@ -7,9 +8,14 @@ class Trade(Base):
     __tablename__ = "trades"
 
     id = Column(Integer, primary_key=True, index=True)
+
     pair = Column(String, nullable=False)
     direction = Column(String, nullable=False)
     entry = Column(Float, nullable=False)
     stop_loss = Column(Float, nullable=False)
     take_profit = Column(Float, nullable=False)
     result = Column(String, nullable=True)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User")
