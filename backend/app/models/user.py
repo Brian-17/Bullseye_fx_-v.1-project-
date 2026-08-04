532902cfb886c7ae71abb1916ef89408a184264e
Brian chelule <116880584+Brian-17@users.noreply.github.com>
2026-08-04 07:07:03 +0300

    Add relationship for trades in User model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.database.database import Base
 
@@ -11,3 +12,5 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    trades = relationship("Trade", back_populates="user")
